b8b4bedbb0845d8ceeeeee9d2bfba78085cbedc2
Alexandr Stepanenko <alexstep07@icloud.com>
2021-02-15 21:33:23 +0200

    show modal

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const seatsForm = document.querySelector('#tickets');
 const seatsCart = document.querySelector('.cart__tickets');
+const cartTotal = document.querySelector('.cart__total');
+const confirmButton = document.querySelector('.cart-btn--submit');
+const cancelButton = document.querySelector('.cart-btn--cancel');
+const modal = document.querySelector('#modal');
+const modalConfirmButton = modal.querySelector('.modal__confirm');
+const modalCancelButton = modal.querySelector('.modal__cancel');
 const selectedSeats = [];
 const PRICE = 5;
 
@@ -17,6 +23,11 @@ const addSeat = (seatToAdd, selectedSeats) => {
 	selectedSeats.push(seatToAdd);
 };
 
+const showTotal = () => {
+    let total = (PRICE * selectedSeats.length).toFixed(2);
+    cartTotal.textContent = `Total: $ ${total}`;
+}
+
 const updateCart = (selectedSeats) => {
 	let seats = createSelectedSeatsTemplates(selectedSeats);
     seatsCart.innerHTML = '';
@@ -26,11 +37,24 @@ const updateCart = (selectedSeats) => {
     if (seats.length === 0) {
         seatsCart.innerHTML = '<p class="cart__empty">No seats selected yet</p>';
     }
+    showTotal();
 };
+
+const clearSelected = (selectedSeats) => {
+    selectedSeats.length = 0;
+}
 
 const removeSeat = (seatToRemove, selectedSeats) => {
     let index = selectedSeats.indexOf(seatToRemove);
     selectedSeats.splice(index, 1);
+}
+
+const showModal = () => {
+    modal.style.display = "flex";
+}
+
+const hideModal = () => {
+    modal.style.display = "none";
 }
 
 seatsForm.addEventListener('click', ({ target }) => {
@@ -44,3 +68,16 @@ seatsForm.addEventListener('click', ({ target }) => {
 		updateCart(selectedSeats);
 	}
 });
+
+confirmButton.addEventListener('click', () => {
+    showModal();
+})
+
+cancelButton.addEventListener('click', () => {
+    clearSelected(selectedSeats);
+    updateCart(selectedSeats);
+});
+
+modalCancelButton.addEventListener('click', () => {
+    hideModal();
+})
